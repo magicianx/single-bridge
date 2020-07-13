74bdc0912f217088b71df2ac0e187ca50625b00b
cludy <cludy.w@gmail.com>
2020-07-13 15:51:29 +0800

    补充订单类型

--- src/main/java/com/gaea/single/bridge/enums/OrderType.java
+++ src/main/java/com/gaea/single/bridge/enums/OrderType.java
@@ -20,7 +20,19 @@ public enum OrderType implements Metadata {
   NEW_RED_PACKET(11, "新手红包"),
   SYSTEM_REISSUE_ORDER(12, "系统补单"),
   PLATFORM_ACTIVITY_PAY(13, "平台活动支出"),
-  PLATFORM_ACTIVITY_INCOME(14, "平台活动收入");
+  PLATFORM_ACTIVITY_INCOME(14, "平台活动收入"),
+  MASONRY_OUT(15, "钻石兑换"),
+  MASONRY_INCOME(16, "钻石兑换"),
+  AUDIO_OUT(17, "语音支出"),
+  AUDIO_INCOME(18, "语音收入"),
+  AUDIO_GRATUITY_PAY(19, "语音礼物支出"),
+  AUDIO_GRATUITY_INCOME(20, "语音礼物收入"),
+  PRIVILEGE_GIVE(21, "购买特殊身份赠送"),
+  DYNAMIC_INCOME(22, "动态广场打赏收入"),
+  DYNAMIC_PAY(23, "动态广场打赏支出"),
+  VOD_INCOME(24, "小视频打赏收入"),
+  VOD_PAY(25, "小视频打赏支出"),
+  BUY_FIRST_RECHARGE_GIFT(26, "购买礼包");
 
   private Integer code;
   private String desc;
