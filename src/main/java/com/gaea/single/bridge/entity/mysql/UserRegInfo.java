f0dba69d6192c788c706c570708215b77d80b20a
cludy <cludy.w@gmail.com>
2020-07-13 14:43:54 +0800

    修复bug

--- src/main/java/com/gaea/single/bridge/entity/mysql/UserRegInfo.java
+++ src/main/java/com/gaea/single/bridge/entity/mysql/UserRegInfo.java
@@ -3,9 +3,7 @@ package com.gaea.single.bridge.entity.mysql;
 import lombok.Data;
 import org.springframework.data.annotation.Id;
 
-import java.time.LocalDate;
 import java.time.LocalDateTime;
-import java.util.Date;
 
 @Data
 public class UserRegInfo {
